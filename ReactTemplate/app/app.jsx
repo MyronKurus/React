3f1c31179ed6021123c24db0fa18c8ae9778c5a5
Myron Kurus <myron.kurus@gmail.com>
2017-02-27 15:15:08 +0200

    React template

---
+++ ReactTemplate/app/app.jsx
@@ -0,0 +1,22 @@
+import React     from 'react';
+import ReactDOM  from 'react-dom';
+import {Router, Route, IndexRoute, hashHistory} from 'react-router';
+
+import Main from 'Main';
+
+//Load foundation
+require('style!css!foundation-sites/dist/css/foundation.min.css');
+$(document).foundation();
+
+// Load styles
+require('style!css!applicationStyles');
+
+const app = document.getElementById('app');
+
+ReactDOM.render(
+  <Router history={hashHistory}>
+      <Route path="/" component={Main}>
+      </Route>
+  </Router>,
+  app
+);
---
+++ ReactTemplate/app/components/Main.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+var Main = (props) => {
+  return (
+    <div>
+      <h1 class="page-title">Hello, React!</h1>
+    </div>
+  );
+};
+
+module.exports = Main;
